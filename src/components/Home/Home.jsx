1cc399fd307ed627054a2c6f48fd250d5800854c
Dillon Coleman <coleman399@gmail.com>
2021-12-08 14:30:14 -0700

    added grid to homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
+import './App.css';
 
-const Home = (props) => {
-    return ( 
-        <div>
-            
-        </div>
-     );
-}
- 
-export default Home;
+
+const homePage = (props) => {
+        return (
+            <div className="container-fluid">
+                <br/>
+                <div className="row">
+                    <div className="col-1"/>
+                    <div className="col-10">
+                        
+                    </div>
+                    <div className="col-1"/>
+                </div>
+                <br/>
+                <div className="row">
+                    <div className="col-1"/>
+                    <div className="col-7">
+                <div className="container">
+                    <div className="row">
+                        
+                        <br/>
+                    </div>
+                    <br/>
+                <div className="row">
+                    <div className="col-1">
+                        <CommentSection {...this.state} getBackendData={this.getBackendData} postBackendData={this.postBackendData}/>
+                    </div>
+                    </div>
+                </div>
+                <div className="col-3">
+                    <VideosPanel {...this.state} searchVideo={this.searchVideo}/>
+                </div>
+                <div className="col-1"/>
+                </div>
+            </div>
+            )
+        }
+
+export default App;
