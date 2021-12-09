dc3f221adc061ba747e78ac5c8ba8cc6865d05fe
Brycen Harmon <brycenharmon@gmail.com>
2021-12-09 10:56:08 -0600

    created template for home, moved navbar component information from home.jsx to NavBar.jsx and created Plant component with template

---
+++ src/components/Plant/Plant.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+function Plant(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          <div>
+            <h4>Name: Plant</h4>
+            <h4>Price: 1</h4>
+            <h4>Description:</h4>
+            <h4>Category:</h4>
+            <h4>Average Rating:</h4>
+            <h4>Reviews:</h4>
+          </div>
+          <form>
+            <button type="submit">Leave a review?</button>
+            <div class="form-group">
+              <label for="exampleFormControlTextarea1">
+                How was your experience?
+              </label>{" "}
+              <br />
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio1"
+                  value="option1"
+                />
+                <label className="form-check-label" for="inlineRadio1">
+                  1
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="option2"
+                />
+                <label className="form-check-label" for="inlineRadio2">
+                  2
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="option2"
+                />
+                <label className="form-check-label" for="inlineRadio2">
+                  3
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="option2"
+                />
+                <label className="form-check-label" for="inlineRadio2">
+                  4
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="inlineRadioOptions"
+                  id="inlineRadio2"
+                  value="option2"
+                />
+                <label className="form-check-label" for="inlineRadio2">
+                  5
+                </label>
+              </div>
+              <textarea
+                class="form-control"
+                id="exampleFormControlTextarea1"
+                rows="3"
+              ></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Plant;
