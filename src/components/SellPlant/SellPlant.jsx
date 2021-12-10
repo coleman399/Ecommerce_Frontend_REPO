7f88a4a286d3e3620c9449f22e0f4661b5ab11b5
Brycen Harmon <brycenharmon@gmail.com>
2021-12-10 11:27:21 -0600

    Created a modal for sellPlant

--- src/components/SellPlant/SellPlant.jsx
+++ src/components/SellPlant/SellPlant.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 const SellPlant = (props) => {
-    return ( 
-        <div>
-            <button>Sell a Plant</button>
-        </div>
-     );
-}
- 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Sell a Plant
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Plant Details:</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Plant Name:</Form.Label>
+              <Form.Control type="text" placeholder="Rose" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Plant Price in Dollars:</Form.Label>
+              <Form.Control type="text" placeholder="40" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Plant Description:</Form.Label>
+              <Form.Control type="text" placeholder="Thorny and red" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Plant Category:</Form.Label>
+              <Form.Select aria-label="Default select example">
+                <option>Select a category:</option>
+                <option value="1">Indoor</option>
+                <option value="2">Outdoor</option>
+              </Form.Select>{" "}
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Submit Plant
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
 export default SellPlant;
