f7572ccd71d723359d7df125d855888d987c313e
Brycen Harmon <brycenharmon@gmail.com>
2021-12-10 13:30:49 -0600

    added slide pane functionality to shapping cart, and added shopping cart to navbar

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component, useState } from "react";
+import { render } from "react-dom";
+import SlidingPane from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
 
 const ShoppingCart = (props) => {
-    return ( 
+    const [state, setState] = useState({
+        isPaneOpen: false,
+        isPaneOpenLeft: false,
+    });
+    
+    return (
         <div>
-            <button>Shopping Cart</button>
-        </div>
-     );
-}
- 
+      <button onClick={() => setState({ isPaneOpen: true })}>
+        Shopping Cart
+      </button>
+      <SlidingPane
+        className="some-custom-class"
+        overlayClassName="some-custom-overlay-class"
+        isOpen={state.isPaneOpen}
+        title="Users Shopping Cart"
+        subtitle="Optional subtitle."
+        onRequestClose={() => {
+          setState({ isPaneOpen: false });
+        }}
+        >
+        <div>This is where your plants are!</div>
+        <br />
+      </SlidingPane>
+    </div>
+  );
+};
 export default ShoppingCart;
